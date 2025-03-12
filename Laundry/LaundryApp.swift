58bf97852074c4f586842afe830f39266171b1c1
Javier Fransiscus <pier9402@gmail.com>
2025-03-12 15:52:22 +0800

    Initial Commit

---
+++ Laundry/LaundryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaundryApp.swift
+//  Laundry
+//
+//  Created by Javier Fransiscus on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct LaundryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
